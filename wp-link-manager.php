8b2a68839794c64b98309d026c22bbacb5981756
fish_ball <57082212@qq.com>
2015-12-16 19:49:59 +0800

    完成了wlm_match_domain 的文章链接解析函数

--- wp-link-manager.php
+++ wp-link-manager.php
@@ -73,6 +73,14 @@ function wlm_settings_page() {
  */
 function parse_wlm_rule_lists() {
 
+    /**
+     * Enqueues the admin script and styles.
+     */
+    if(is_admin()) {
+        wp_enqueue_style('wlm', WLM__PLUGIN_URL.'_inc/wp-link-manager.css');
+        wp_enqueue_script('wlm', WLM__PLUGIN_URL.'_inc/wp-link-manager.js', array('jquery'));
+    }
+
     global $wlm_white_list;
     $wlm_white_list = array();
     $opt_white = get_option('wlm_domain_white_list', '');
@@ -156,11 +164,107 @@ function wlm_is_domain_allowed($domain) {
 }
 
 /**
- * Enqueues the admin script and styles.
+ * Matches the link position in the content text.
+ * @param string $content: The full text to search
+ * @param string|array $domains: The object domains matching
+ *      default: use all suspected domains list.
+ * @param string $marked_content returns the content,
+ *      with its matched <a> tag replaced of {% wlm_link <index> %}
+ *      content, where index equals the result array list index
+ *      starts at 0.
+ * @return array: Returns the resulting objects of all matches:
+ * Each element contains:
+ * array(
+ *      'markup' => the whole content of the tag.
+ *      'url' => the matching url, i.e. the href attribute of the <a> tag.
+ *      'domain' => the matching domain
+ *      'attributes' => the attributes of the <a> tag.
+ *      'content' => the content of the <a> tag.
+ * )
  */
-if(is_admin()) {
-    add_action('admin_init', function() {
-        wp_enqueue_style('wlm', WLM__PLUGIN_URL.'_inc/wp-link-manager.css');
-        wp_enqueue_script('wlm', WLM__PLUGIN_URL.'_inc/wp-link-manager.js', array('jquery'));
-    });
+function wlm_match_domain($content, $domains=null, &$marked_content='') {
+
+    include_once 'phpQuery/phpQuery.php';
+
+    // Normalize domain type
+    if(is_string($domains)) $domain = explode('|', $domains);
+
+    // Scratch all the href attribute of <a> tags.
+    preg_match_all(
+        "/<\\s*a[^<>]+href=[\"']([^\"']+)[\"'].+?(?:\\/>|<\\/\\s*a\\s*>)/ims",
+        $content, $matches, PREG_PATTERN_ORDER
+    );
+
+    $result = array();
+
+    // Loops into each link in the post.
+    for($i = 1; $i < sizeof($matches[1]); ++$i) {
+
+        // Parsing the link info.
+        $markup = $matches[0][$i];
+        $url = $matches[1][$i];
+        $url_info = parse_url($url);
+
+        // No scheme or no hosts links are treated as safe.
+        if(!isset($url_info['scheme']) || !isset($url_info['host'])) continue;
+
+        $scheme = $url_info['scheme'];  // http or https
+        $host = @$url_info['host'];  // domain name and port
+        $path = @$url_info['path'] ?: '/';
+
+        // Host in specified domains or not allowed by WLM strategy.
+        $is_host_matched = $domains && in_array($host, $domains)
+            || !$domains && !wlm_is_domain_allowed($host);
+
+        // If matched, parse the link info.
+        if($is_host_matched) {
+            // phpQuery document
+            phpQuery::newDocument($markup);
+            $a = pq('a');
+            $result []= array(
+                'markup' => $markup,
+                'url' => $url,
+                'domain' => $host,
+                'attributes' => $a->attr('*'),
+                'content' => $a->html(),
+            );
+        }
+
+    }
+
+    // Replace the $marked_content as number tags.
+    $marked_content = $content;
+    foreach($result as $i => $item) {
+        $pos = strpos($marked_content, $item['markup']);
+        if($pos !== false) {
+            $marked_content = substr_replace(
+                $marked_content,
+                "{% wlm_link $i %}",
+                $pos,
+                strlen($item['markup'])
+            );
+        }
+    }
+
+    return $result;
+
 }
+
+
+
+/**
+ *
+ * @param int $post_id: The id of the post
+ * @param string $domain: Which domains of link will be affected
+ * @param string $action: The action type:
+ *      + TODO: remove: remove the entire <a> tag including the content
+ *      + TODO: strip_tag: remove the <a> tag but keeps the content
+ *      + TODO: set_null: set the href attribute to #
+ *      + TODO: nofollow: keep the markup but adding no-follow attr on it
+ */
+function wlm_do_link_action($post_id, $domain, $action='remove') {
+
+
+}
+
+
